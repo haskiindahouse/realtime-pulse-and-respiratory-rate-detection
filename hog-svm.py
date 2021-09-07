7ceb6ae2e851db4076405fbc85c89ff8a5b25c64
Mikhail Murunov <haskiindahouse@gmail.com>
2021-09-07 22:01:42 +0300

    [ui] Добавил простой UI.

--- hog-svm.py
+++ hog-svm.py
@@ -2,25 +2,12 @@ import face_recognition
 import cv2
 import numpy as np
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-# Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
-# Load a sample picture and learn how to recognize it.
-user_image = face_recognition.load_image_file("/Users/mihailmurunov/PycharmProjects/realtime-pulse-and-respiratory"
-                                               "-rate-detection/users/Mikhail.png")
+user_image = face_recognition.load_image_file("/users/Mikhail.png")
 user_face_encoding = face_recognition.face_encodings(user_image)[0]
 
-
-# Create arrays of known face encodings and their names
 known_face_encodings = [
     user_face_encoding,
 
@@ -29,7 +16,6 @@ known_face_names = [
     "Mikhail Murunov",
 ]
 
-# Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
