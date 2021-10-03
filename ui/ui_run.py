30d6bb332109ff0c468b86050ebd279146224e65
Mikhail Murunov <haskiindahouse@gmail.com>
2021-10-03 20:37:51 +0300

    Revert "[ui][db][main] Доработка интерфейса и начало интеграции с бд."
    This reverts commit 827aa991942642d05ded4fe89bd99e2519d88a32.

--- ui/ui_run.py
+++ ui/ui_run.py
@@ -1,17 +1,14 @@
 import sys
-import os
 
 import cv2
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIcon
 
 from ui_main import Ui
 
 
 def initUi():
     app = QApplication(sys.argv)
-    path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'sources/app_logo.png')
-    app.setWindowIcon(QIcon(path))
+
     ex = Ui()
 
     sys.exit(app.exec_())
